05b7ac2e424b0a59272965a467eea0d69d949263
idanran <idanran@outlook.com>
2022-04-26 20:07:37 +0800

    0.1.2

--- plugin/ping.js
+++ plugin/ping.js
@@ -2,7 +2,8 @@ export const type = 'message';
 export const name = 'ping';
 
 export function apply({ session, config, segment }) {
+  // 如果收到“天王盖地虎”，就回应“宝塔镇河妖”
   if (session.content === "天王盖地虎") {
-    session.send("宝塔镇河妖");
+    session.send("宝塔镇河妖")
   }
 }
